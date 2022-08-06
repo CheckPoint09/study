a01b4c41a2afdc70eff19da9f8bb6495c1a88e73
Vlad <v.chekcpoint09@gmail.com>
2022-08-06 23:24:07 +0300

    HW3

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?", ''),
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    }
+}
+start();
+
 
 let appData = {
 	budget: money,
@@ -9,67 +17,33 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true
 };
 
 
 
 
-/*for(let i=0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ' '),
-	    b = prompt("Во сколько обойдется?", ' ');
-    
-        if ((typeof(a))  === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) { 
+function chooseExpenses() {
+    for(let i=0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ' '),
+            b = prompt("Во сколько обойдется?", ' ');
         
-            appData.expenses[a] = b; 
-            } else {
+            if ((typeof(a))  === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) { 
             
-            console.log("Вы ввели неверные значения");
-
-            i--;
-           
-        };
-      
-    }*/
-    /*let i = 0;
-    
-    while(i < 2) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ' '),
-	        b = prompt("Во сколько обойдется?", ' ');
-           
-            if ((typeof(a))  === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) { 
-        
                 appData.expenses[a] = b; 
                 } else {
                 
                 console.log("Вы ввели неверные значения");
     
                 i--;
-                }
-            i++;
-    };*/
-    let i = 0;
+               
+            }
+          
+        }
+}
+chooseExpenses();
 
-    do {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ' '),
-	        b = prompt("Во сколько обойдется?", ' ');
-           
-            if ((typeof(a))  === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) { 
-        
-                appData.expenses[a] = b; 
-                } else {
-                
-                console.log("Вы ввели неверные значения");
-    
-                i--;
-                }
-            i++;
-    }
-    while(i < 2);
-    
-
-    
-appData.moneyPerDay = alert(appData.budget / 30);
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 alert("Ежедневный бюджет" + appData.moneyPerDay);
 
@@ -86,3 +60,16 @@ if ( appData.moneyPerDay < 100 ) {
     console.log("Что то пошло не так");
 
 };
+
+
+function checkSavings(){
+    if (appData.savings == true){
+        let save = +prompt("Kakova sunna nokoplenij?"),
+        percent = +prompt("Pod kakoj percent");
+
+        appData.monthIncome = save/100/12*percent;
+        alert("Dohod v mesyac s depozita: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
